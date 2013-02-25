30361b25298d7e9e0d89907525346f7799997f3c
Kzimer <kzimer@zoho.com>
2013-02-25 09:04:58 +0100

    solved problem 009

---
+++ 009/009.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from math import sqrt
+
+def isSquare(x):
+    return x == (int(sqrt(x))**2)
+
+def sumTriplet(a, b):
+    if isSquare(a**2 + b**2):
+        return a + b + int(sqrt(a**2 + b**2))
+    return 0
+
+a = 3
+
+while (a < 333):
+    b = a + 1
+    while(b < 334):
+        if (sumTriplet(a, b) == 1000):
+            #print a * b * (sumTriplet(a, b) - a - b)
+            break
+        b += 1
+    a += 1
